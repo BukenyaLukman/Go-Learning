797f3f0ca978b9ac73985bdb9aee3bfc7881dd89
BukenyaLukman <bukenyalukman@gmail.com>
2020-07-16 12:51:27 +0300

    Constants in go

---
+++ go5/Constants.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+
+func main(){
+
+	const firstConstant float64 = 12
+	const b float64 = 3.14
+	const c string = "Lukman"
+	const d bool = true
+	
+
+
+	fmt.Printf("%v\n",b)
+	fmt.Printf("%v\n",c)
+	fmt.Printf("%v\n",d)
+	// fmt.Printf("%v\n", firstConstant)
+	// fmt.Printf("%v\n", firstConstant)
+}
